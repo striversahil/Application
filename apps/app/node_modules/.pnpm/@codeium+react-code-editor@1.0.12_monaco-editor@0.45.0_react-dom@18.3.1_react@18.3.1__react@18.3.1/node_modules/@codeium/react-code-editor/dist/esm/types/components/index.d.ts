02e7d218d988a5ba42207761ecafb86fba10afdc
Sahil Kumar Jha <striversahil@gmail.com>
2025-03-11 17:36:57 +0530

    added backend codebase

--- apps/app/node_modules/.pnpm/@codeium+react-code-editor@1.0.12_monaco-editor@0.45.0_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@codeium/react-code-editor/dist/esm/types/components/index.d.ts
+++
@@ -1,1 +0,0 @@
-export * from './CodeiumEditor/CodeiumEditor';
--- apps/app/node_modules/.pnpm/@codeium+react-code-editor@1.0.12_monaco-editor@0.45.0_react-dom@19.0.0_react@19.0.0__react@19.0.0/node_modules/@codeium/react-code-editor/dist/esm/types/models/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Language } from '../api/proto/exa/codeium_common_pb/codeium_common_pb';
-export { Document } from '../api/proto/exa/language_server_pb/language_server_pb';
